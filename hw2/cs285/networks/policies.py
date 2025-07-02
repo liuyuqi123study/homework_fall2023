8618e95c13f0aff65821e4e923bcdfbc75aeeeaa
Yuqi Liu <123962595+liuyuqi123study@users.noreply.github.com>
2025-07-02 16:11:43 +0800

    woo

--- hw2/cs285/networks/policies.py
+++ hw2/cs285/networks/policies.py
@@ -59,8 +59,7 @@ class MLPPolicy(nn.Module):
     def get_action(self, obs: np.ndarray) -> np.ndarray:
         """Takes a single observation (as a numpy array) and returns a single action (as a numpy array)."""
         # TODO: implement get_action
-        action = None
-
+        action = self.forward(obs)
         return action
 
     def forward(self, obs: torch.FloatTensor):
@@ -71,14 +70,16 @@ class MLPPolicy(nn.Module):
         """
         if self.discrete:
             # TODO: define the forward pass for a policy with a discrete action space.
-            pass
+            prob=self.logits_net(obs)
         else:
             # TODO: define the forward pass for a policy with a continuous action space.
-            pass
-        return None
+            mean=self.mean_net(obs)
+            prob=self.logstd(mean)
+        return prob
 
     def update(self, obs: np.ndarray, actions: np.ndarray, *args, **kwargs) -> dict:
         """Performs one iteration of gradient descent on the provided batch of data."""
+        
         raise NotImplementedError
 
 
@@ -97,8 +98,10 @@ class MLPPolicyPG(MLPPolicy):
         advantages = ptu.from_numpy(advantages)
 
         # TODO: implement the policy gradient actor update.
-        loss = None
-
+        loss=-(actions*advantages).mean()
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
         return {
             "Actor Loss": ptu.to_numpy(loss),
         }
